607f3ddec17abd6befa1f2f0ea2ebf31211b0e80
talebFrederik <taleb.frederik@cegepvicto.ca>
2021-11-15 15:48:27 -0500

    Correction génération des classes

--- Program.cs
+++ Program.cs
@@ -6,10 +6,6 @@ namespace Labo_TDZ_Final00
     {
         static void Main(string[] args)
         {
-            
-
-            Console.WriteLine("Bienvenue à la tour de Zork, jusqu'où pourras-tu aller?\n  1.Nouvelle partie\n  2.Quitter");
-            GenerateurClasse generateurClasse = new GenerateurClasse();
 
         }
     }
